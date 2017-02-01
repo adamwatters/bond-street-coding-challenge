f6b9416153dcc5c855ae5e1eaf25f684684da923
Adam <adamwatters11@gmail.com>
2017-02-01 11:20:35 -0500

    user model and test

---
+++ app/test/models/user_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "user")
+    @other_user = User.new(name: "other user")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should be unique" do
+    @other_user.name = @user.name
+    assert @user.save
+    assert_not @other_user.save
+  end
+
+end
